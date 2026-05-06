83cd6cbd59af7b68975883c5e8a68d7146730633
an-jorge <an.jorge@icloud.com>
2026-05-06 05:22:02 +0100

    Import_change_app_package_name

--- hexatrack/test/widget_test.dart
+++ hexatrack/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hexatrack/main.dart';
+import 'package:fw_tool/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
